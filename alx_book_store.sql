9b4a9383ca609c1a16215e538df613cafd254c42
Touracht <tebogomataba6810@gmail.com>
2024-07-27 19:00:45 +0200

    Add alx_book_store.sql to INTRO_TO_DB

---
+++ alx_book_store.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS  alx_book_store;
+
+USE alx_book_store;
+CREATE TABLE IF NOT EXISTS Books(
+    book_id Primary KEY,
+    title VARCHAR(130),
+        author_id,
+        FOREIGN KEY author_id REFERENCES Authors(author_id),
+        price DOUBLE,
+        publication_date DATE
+);
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id PRIMARY KEY,
+    customer_id,
+    customer_id REFERENCES Customers(customer_id)
+    order_date DATE
+);
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid PRIMARY KEY,
+    order_id,
+    order_id REFERENCES Orders(order_id)
+    book_id,
+    book_id REFERENCES Books(book_id)
+);
+
