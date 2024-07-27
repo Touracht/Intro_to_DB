751032034769efd808b65a3e635f865cf116b571
Touracht <tebogomataba6810@gmail.com>
2024-07-27 19:20:35 +0200

    Add alx_book_store.sql to INTRO_TO_DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,14 +21,14 @@ CREATE TABLE IF NOT EXISTS Customers(
 CREATE TABLE IF NOT EXISTS Orders(
     order_id PRIMARY KEY,
     customer_id,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid PRIMARY KEY,
     order_id INT,
-    FOREIGN KEY(order_id) INT REFERENCES Orders(order_id),
+    FOREIGN KEY (order_id) INT REFERENCES Orders(order_id),
     book_id INT,
-    FOREIGN KEY(book_id) INT REFERENCES Books(book_id)
+    FOREIGN KEY (book_id) INT REFERENCES Books(book_id)
 );
 
